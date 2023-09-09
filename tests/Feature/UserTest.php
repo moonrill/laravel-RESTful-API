9be7b167157a9636c766de6f1039ed7c97587320
moonrill <arilramadani245@gmail.com>
2023-09-09 10:04:16 +0700

    membuat register user api

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testRegisterSuccess()
+    {
+        $this->post('/api/users', [
+            'username' => 'aaron',
+            'password' => 'rahasia',
+            'name' => 'Aaron Moon'
+        ])->assertStatus(201)
+            ->assertJson([
+                "data" => [
+                    'username' => 'aaron',
+                    'name' => 'Aaron Moon'
+                ]
+            ]);
+    }
+
+    public function testRegisterFailed()
+    {
+        $this->post('/api/users', [
+            'username' => '',
+            'password' => '',
+            'name' => ''
+        ])->assertStatus(400)
+            ->assertJson([
+                "errors" => [
+                    'username' => [
+                        "The username field is required."
+                    ],
+                    'password' => [
+                        "The password field is required."
+                    ],
+                    'name' => [
+                        "The name field is required."
+                    ]
+                ]
+            ]);
+    }
+
+    public function testRegisterUsernameAlreadyExists ()
+    {
+        $this->testRegisterSuccess();
+        $this->post('/api/users', [
+            'username' => 'aaron',
+            'password' => 'rahasia',
+            'name' => 'Aaron Moon'
+        ])->assertStatus(400)
+            ->assertJson([
+                "errors" => [
+                    'username' => [
+                        "username already registered"
+                    ]
+                ]
+            ]);
+    }
+
+
+}
