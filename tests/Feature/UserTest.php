c48806170d37af302f78febd16767778476d32c5
moonrill <arilramadani245@gmail.com>
2023-09-09 20:53:37 +0700

    membuat user logout api dan test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -226,4 +226,35 @@ class UserTest extends TestCase
             ]);
 
     }
+
+    public function testLogoutSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->delete('/api/users/logout', headers: [
+            'Authorization' => 'test'
+        ])->assertStatus(200)
+            ->assertJson([
+                'data' => true
+            ]);
+
+        $user = User::where('username', 'test')->first();
+        self::assertNull($user->token);
+    }
+
+    public function testLogoutFailed()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->delete('/api/users/logout', headers: [
+            'Authorization' => 'salah'
+        ])->assertStatus(401)
+            ->assertJson([
+                'errors' => [
+                    'message' => [
+                        'Unauthorized'
+                    ]
+                ]
+            ]);
+    }
 }
