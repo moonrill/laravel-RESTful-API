629d1bbc5dc8ef6e4c60c93fd7bc863272678611
moonrill <arilramadani245@gmail.com>
2023-09-10 12:46:49 +0700

    membuat update contact api

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactCreateRequest;
+use App\Http\Requests\ContactUpdateRequest;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -39,5 +40,27 @@ class ContactController extends Controller
         }
 
     return new ContactResource($contact);
+    }
+
+    public function update(int $id, ContactUpdateRequest $request):ContactResource
+    {
+        $user = Auth::user();
+
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+        if (!$contact){
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => [
+                        "User not found"
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+
+        $data = $request->validated();
+        $contact->fill($data);
+        $contact->save();
+
+        return new ContactResource($contact);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -33,4 +33,5 @@ Route::middleware(ApiAuthMiddleware::class)->group(function (){
     // Contact
     Route::post('/contacts', [ContactController::class, 'create']);
     Route::get('/contacts/{id}', [ContactController::class, 'get'])->where('id', '[0-9]+');
+    Route::put('/contacts/{id}', [ContactController::class, 'update'])->where('id', '[0-9]+');
 });
